434a1f7c0e14d136a3c8cdb1f79b23dc9cc646a1
IrisWongSF <130765407+IrisWongSF@users.noreply.github.com>
2023-04-27 22:41:18 -0400

    Add files via upload
    updated

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -7,7 +7,6 @@ def main():
     # Set up game
 
     WHITE = (255, 255, 255)
-    x = 10
     WIN_WIDTH = 300
     WIN_HEIGHT = 300
 
@@ -31,29 +30,25 @@ def main():
 
         WIN.fill(WHITE)
         for i in range(1, 3):
-            """ This for loop is for drawing lines """
+            # This for loop is for drawing lines
             pygame.draw.line(WIN, grid_colour, (0, i * 100), (WIN_WIDTH, i * 100), line_width)
             pygame.draw.line(WIN, grid_colour, (i * 100, 0), (i * 100, WIN_HEIGHT), line_width)
 
     for i in range(3):
-        row = [0] * 3
+        row = [0,0,0]
         board.append(row)
 
     run = True
     clicked = False
     draw_grid()
+
     while run:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-            if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
-                clicked = True
-                print("clicked")
-            if event.type == pygame.MOUSEBUTTONUP and clicked == True:
-                clicked = False
-                print("clicked2")
+            if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
                 cell_x = pos[0]
                 cell_y = pos[1]
@@ -61,7 +56,6 @@ def main():
                 if board[cell_y // 100][cell_x // 100] == 0:
 
                     board[cell_y // 100][cell_x // 100] = turn
-                    print(board)
 
                     if turn == "X":
 
@@ -71,17 +65,14 @@ def main():
 
                     cell_x, cell_y = check_pos(cell_x, cell_y)
 
-                    print("cell_x", cell_x)
-                    print("cell_y", cell_y)
-
                     WIN.blit(pic, (cell_x, cell_y))
 
                     pygame.display.flip()
-                    # check_winning_condition(board)
+                    #check_winning_condition(board)
                     if check_winning_condition(board) or check_draw(board):
                         msg = ''
                         if check_winning_condition(board):
-                            msg = "The winner is " + turn
+                            msg = f"The winner is {turn}"
                         else:
                             msg = "Draw !"
                         font = pygame.font.Font(None, 36)
@@ -99,6 +90,7 @@ def main():
                             for event in pygame.event.get():
                                 if event.type == pygame.QUIT:
                                     pygame.quit()
+
                         break
 
                     else:
@@ -113,48 +105,37 @@ def main():
 
 
 def check_pos(cell_x, cell_y):
-    if cell_x // 100 == 0 and cell_y // 100 == 0:
-        print("opt 1 ")
+    if (cell_x // 100 == 0 and cell_y // 100 == 0):
         cell_x = 20
         cell_y = 30
-    elif cell_x // 100 == 0 and cell_y // 100 == 1:
-        print("opt 2 ")
+    elif (cell_x // 100 == 0 and cell_y // 100 == 1):
         cell_x = 30
         cell_y = 130
-    elif cell_x // 100 == 0 and cell_y // 100 == 2:
-        print("opt 3 ")
+    elif (cell_x // 100 == 0 and cell_y // 100 == 2):
         cell_x = 30
         cell_y = 230
-    elif cell_x // 100 == 1 and cell_y // 100 == 0:
-        print("opt 4 ")
+    elif (cell_x // 100 == 1 and cell_y // 100 == 0):
         cell_x = 130
         cell_y = 30
-    elif cell_x // 100 == 1 and cell_y // 100 == 1:
-        print("opt 5 ")
+    elif (cell_x // 100 == 1 and cell_y // 100 == 1):
         cell_x = 130
         cell_y = 130
-    elif cell_x // 100 == 1 and cell_y // 100 == 2:
-        print("opt 6 ")
+    elif (cell_x // 100 == 1 and cell_y // 100 == 2):
         cell_x = 130
         cell_y = 230
-    elif cell_x // 100 == 2 and cell_y // 100 == 0:
-        print("opt 7 ")
+    elif (cell_x // 100 == 2 and cell_y // 100 == 0):
         cell_x = 230
         cell_y = 30
-    elif cell_x // 100 == 2 and cell_y // 100 == 1:
-        print("opt 8 ")
+    elif (cell_x // 100 == 2 and cell_y // 100 == 1):
         cell_x = 230
         cell_y = 130
-    elif cell_x // 100 == 2 and cell_y // 100 == 2:
-        print("opt 9 ")
+    elif (cell_x // 100 == 2 and cell_y // 100 == 2):
         cell_x = 230
         cell_y = 230
     return cell_x, cell_y
 
 
 def check_winning_condition(list):
-    print(list)
-    print(((list[0][0] == list[0][1] == list[0][2])))
     if ((((list[0][0] == list[0][1] == list[0][2])) and list[0][0] != 0)
             | (((list[1][0] == list[1][1] == list[1][2])) and list[1][0] != 0)
             | (((list[2][0] == list[2][1] == list[2][2])) and list[2][0] != 0)
